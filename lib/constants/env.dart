44bb741e55d7baf7bbede215ff66c3d30ba42c2f
saloluwaseun@gmail.com <saloluwaseun@gmail.com>
2023-04-02 14:25:20 +0100

    farmx-firstcommit

---
+++ lib/constants/env.dart
@@ -0,0 +1,9 @@
+class Env {
+  static const String analysis_baseurl =
+      "https://cropanalysis-zc6ytsujkq-uw.a.run.app/";
+
+  static const String recommendation_url =
+      "https://croprecommendation-zc6ytsujkq-uw.a.run.app/";
+
+  static const String gcp_key = "AIzaSyAWRghVw_DAzqKW5SJDsLG9AMUidb0pr-0";
+}
